4ee6cfd993954068ffc286bc617068cbafe17097
Alexander4649 <y-teshigawara@braveact1506.onmicrosoft.com>
2023-07-28 17:15:19 +0900

    HIGHLOWまで修正コミット

---
+++ sunsudrill/highlow.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace highandlow
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// 必要なデザイナー変数です。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 使用中のリソースをすべてクリーンアップします。
+        /// </summary>
+        /// <param name="disposing">マネージド リソースを破棄する場合は true を指定し、その他の場合は false を指定します。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows フォーム デザイナーで生成されたコード
+
+        /// <summary>
+        /// デザイナー サポートに必要なメソッドです。このメソッドの内容を
+        /// コード エディターで変更しないでください。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.AutoSize = true;
+            this.button1.Location = new System.Drawing.Point(256, 291);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(79, 42);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "HIGH";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.AutoSize = true;
+            this.button2.Location = new System.Drawing.Point(409, 291);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(86, 42);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "LOW";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("MS UI Gothic", 30F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label1.Location = new System.Drawing.Point(351, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(179, 63);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "label1";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label2.Location = new System.Drawing.Point(252, 108);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(66, 23);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "label2";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // button3
+            // 
+            this.button3.AutoSize = true;
+            this.button3.Location = new System.Drawing.Point(341, 361);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 31);
+            this.button3.TabIndex = 4;
+            this.button3.Text = "リセット";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(228, 365);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(43, 15);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "lavel3";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(492, 365);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(43, 15);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "lavel4";
+            this.label4.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("MS UI Gothic", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.label5.Location = new System.Drawing.Point(312, 48);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(66, 23);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "label5";
+            this.label5.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoSize = true;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "ハイ＆ロー";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+    }
+}
+
---
+++ sunsudrill/highlow.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace highandlow
+{
+    public partial class Form1 : Form
+    {
+        Random rand = new Random(); //乱数定義
+        long kazu = 0; //変数初期
+        long good = 0; //当たりカウント初期
+        long bad = 0; //外れカウント初期
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // HIGHボタンが押されたとき
+            if(kazu >= 5)
+            {
+                label2.Text = "あたり";
+                good++;
+            }
+            else
+            {
+                label2.Text = "はずれ";
+                bad++;
+            }
+            
+            label1.Text = kazu.ToString(); //変数に入った乱数を表示
+            button1.Enabled = false; //リセット押下時ボタンを無効に
+            button2.Enabled = false; //リセット押下時ボタンを無効に
+            button3.Enabled = true;  //選択されたらリセットが表示
+            label3.Text = "当たり回数：" + good.ToString();
+            label4.Text = "外れ回数：" + bad.ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            //LOWを押したとき実行
+            if (kazu <= 5) 
+            {
+                label2.Text = "あたり";
+                good++;
+            }
+            else
+            {
+                label2.Text = "はずれ";
+                bad++;
+            }
+            label1.Text = kazu.ToString(); //変数に入った乱数を表示
+            button1.Enabled = false; //リセット押下時ボタンを無効に
+            button2.Enabled = false; //リセット押下時ボタンを無効に
+            button3.Enabled = true;  //選択されたらリセットが表示
+            label3.Text = "当たり回数：" + good.ToString();
+            label4.Text = "外れ回数：" +  bad.ToString();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+            label1.Text = kazu.ToString(); //変数に入った乱数を表示
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            init(); //initメソッドから参照
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            init();　//initメソッドから参照
+        }
+
+        private void init()
+        {
+            label1.Text = "?"; //初期は？
+            label2.Text = "?の数は5より大きいか小さいか"; //問題の問をラベル２に
+            label3.Text = "当たり回数：" + good.ToString();
+            label4.Text = "外れ回数：" + bad.ToString();
+            label5.Text = "ハイ＆ローゲーム";
+
+            kazu = rand.Next(1, 10); //乱数の範囲を指定
+            Console.WriteLine("乱数" + kazu); //乱数の結果を出力コンソールに表示
+
+            button1.Enabled = true; //リセット押下時ボタンを有効に
+            button2.Enabled = true; //リセット押下時ボタンを有効に
+            button3.Enabled = false;//選択されるまでリセットは押せない
+          //testtesttest
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+
+        private void label4_Click(object sender, EventArgs e)
+        {
+            init();
+        }
+    }
+}

d7c6328129dc669267e4aaa76d796ce9ac8a2159
Alexander4649 <y-teshigawara@braveact1506.onmicrosoft.com>
2023-07-31 13:13:46 +0900

    算数終了

--- sunsudrill/Form1.Designer.cs
+++ sunsudrill/Form1.Designer.cs
@@ -31,13 +31,24 @@
             this.label1 = new System.Windows.Forms.Label();
             this.InBOX = new System.Windows.Forms.TextBox();
             this.OutBox = new System.Windows.Forms.TextBox();
+            this.StartBtn = new System.Windows.Forms.Button();
+            this.MondaiLbl = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.RBhiku = new System.Windows.Forms.RadioButton();
+            this.RBtasu = new System.Windows.Forms.RadioButton();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.RB30 = new System.Windows.Forms.RadioButton();
+            this.RB20 = new System.Windows.Forms.RadioButton();
+            this.RB10 = new System.Windows.Forms.RadioButton();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.label1.Location = new System.Drawing.Point(183, 253);
+            this.label1.Location = new System.Drawing.Point(32, 267);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(60, 27);
             this.label1.TabIndex = 0;
@@ -46,33 +57,146 @@
             // InBOX
             // 
             this.InBOX.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
-            this.InBOX.Location = new System.Drawing.Point(270, 253);
+            this.InBOX.Location = new System.Drawing.Point(119, 267);
             this.InBOX.Name = "InBOX";
-            this.InBOX.Size = new System.Drawing.Size(194, 34);
+            this.InBOX.Size = new System.Drawing.Size(114, 34);
             this.InBOX.TabIndex = 1;
             this.InBOX.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
             this.InBOX.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.InBOX_KeyPress);
             // 
             // OutBox
             // 
-            this.OutBox.Location = new System.Drawing.Point(514, 119);
+            this.OutBox.BackColor = System.Drawing.Color.White;
+            this.OutBox.Font = new System.Drawing.Font("MS UI Gothic", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.OutBox.Location = new System.Drawing.Point(289, 133);
             this.OutBox.Multiline = true;
             this.OutBox.Name = "OutBox";
             this.OutBox.ReadOnly = true;
-            this.OutBox.Size = new System.Drawing.Size(156, 168);
+            this.OutBox.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.OutBox.Size = new System.Drawing.Size(227, 168);
             this.OutBox.TabIndex = 2;
+            // 
+            // StartBtn
+            // 
+            this.StartBtn.Location = new System.Drawing.Point(37, 133);
+            this.StartBtn.Name = "StartBtn";
+            this.StartBtn.Size = new System.Drawing.Size(196, 37);
+            this.StartBtn.TabIndex = 3;
+            this.StartBtn.Text = "開始";
+            this.StartBtn.UseVisualStyleBackColor = true;
+            this.StartBtn.Click += new System.EventHandler(this.StartBtn_Click);
+            // 
+            // MondaiLbl
+            // 
+            this.MondaiLbl.BackColor = System.Drawing.Color.White;
+            this.MondaiLbl.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.MondaiLbl.Font = new System.Drawing.Font("MS UI Gothic", 16F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(128)));
+            this.MondaiLbl.Location = new System.Drawing.Point(37, 192);
+            this.MondaiLbl.Name = "MondaiLbl";
+            this.MondaiLbl.Size = new System.Drawing.Size(196, 57);
+            this.MondaiLbl.TabIndex = 4;
+            this.MondaiLbl.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.MondaiLbl.UseMnemonic = false;
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.RBhiku);
+            this.groupBox1.Controls.Add(this.RBtasu);
+            this.groupBox1.Location = new System.Drawing.Point(37, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(196, 41);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "タイプ";
+            // 
+            // RBhiku
+            // 
+            this.RBhiku.AutoSize = true;
+            this.RBhiku.Location = new System.Drawing.Point(118, 16);
+            this.RBhiku.Name = "RBhiku";
+            this.RBhiku.Size = new System.Drawing.Size(69, 19);
+            this.RBhiku.TabIndex = 7;
+            this.RBhiku.Text = "引き算";
+            this.RBhiku.UseVisualStyleBackColor = true;
+            this.RBhiku.CheckedChanged += new System.EventHandler(this.radioButton2_CheckedChanged);
+            // 
+            // RBtasu
+            // 
+            this.RBtasu.AutoSize = true;
+            this.RBtasu.Checked = true;
+            this.RBtasu.Location = new System.Drawing.Point(32, 16);
+            this.RBtasu.Name = "RBtasu";
+            this.RBtasu.Size = new System.Drawing.Size(69, 19);
+            this.RBtasu.TabIndex = 7;
+            this.RBtasu.TabStop = true;
+            this.RBtasu.Text = "足し算";
+            this.RBtasu.UseVisualStyleBackColor = true;
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.RB30);
+            this.groupBox2.Controls.Add(this.RB20);
+            this.groupBox2.Controls.Add(this.RB10);
+            this.groupBox2.Location = new System.Drawing.Point(37, 70);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(267, 46);
+            this.groupBox2.TabIndex = 6;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "問題数";
+            this.groupBox2.Enter += new System.EventHandler(this.groupBox2_Enter);
+            // 
+            // RB30
+            // 
+            this.RB30.AutoSize = true;
+            this.RB30.Location = new System.Drawing.Point(193, 16);
+            this.RB30.Name = "RB30";
+            this.RB30.Size = new System.Drawing.Size(59, 19);
+            this.RB30.TabIndex = 9;
+            this.RB30.Text = "30問";
+            this.RB30.UseVisualStyleBackColor = true;
+            this.RB30.CheckedChanged += new System.EventHandler(this.radioButton5_CheckedChanged);
+            // 
+            // RB20
+            // 
+            this.RB20.AutoSize = true;
+            this.RB20.Location = new System.Drawing.Point(118, 16);
+            this.RB20.Name = "RB20";
+            this.RB20.Size = new System.Drawing.Size(59, 19);
+            this.RB20.TabIndex = 7;
+            this.RB20.Text = "20問";
+            this.RB20.UseVisualStyleBackColor = true;
+            // 
+            // RB10
+            // 
+            this.RB10.AutoSize = true;
+            this.RB10.Checked = true;
+            this.RB10.Location = new System.Drawing.Point(32, 16);
+            this.RB10.Name = "RB10";
+            this.RB10.Size = new System.Drawing.Size(59, 19);
+            this.RB10.TabIndex = 8;
+            this.RB10.TabStop = true;
+            this.RB10.Text = "10問";
+            this.RB10.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(640, 360);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.MondaiLbl);
+            this.Controls.Add(this.StartBtn);
             this.Controls.Add(this.OutBox);
             this.Controls.Add(this.InBOX);
             this.Controls.Add(this.label1);
             this.Name = "Form1";
             this.Text = "算数ドリル";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -83,6 +207,15 @@
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.TextBox InBOX;
         private System.Windows.Forms.TextBox OutBox;
+        private System.Windows.Forms.Button StartBtn;
+        private System.Windows.Forms.Label MondaiLbl;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.RadioButton RBhiku;
+        private System.Windows.Forms.RadioButton RBtasu;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.RadioButton RB20;
+        private System.Windows.Forms.RadioButton RB10;
+        private System.Windows.Forms.RadioButton RB30;
     }
 }
 
--- sunsudrill/Form1.cs
+++ sunsudrill/Form1.cs
@@ -12,24 +12,150 @@ namespace sunsudrill
 {
     public partial class Form1 : Form
     {
+        Random random = new Random(); //乱数定義
+        long noA = 0; //変数問題A初期
+        long noB = 0; //変数問題B初期
+        long noC = 0; //変数問題C初期
+        long mondaiCnt = 0; //出題数カウント
+        int mondaiMax = 0; //最大出題数
+        int seikaiCnt = 0; //正解数
+        //double seikairitu = 0; //正解率
+        long type = 0; //1:足し算、2:引き算
+        string typeStr; //演算記号表示用
+
         public Form1()
         {
             InitializeComponent();
         }
 
+        private void InBOX_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if((e.KeyChar < '0' || e.KeyChar >'9') && (e.KeyChar != '\b')) //文字コード表0~9の範囲 かつ バックスペース以外の時
+            {
+                if(!(e.KeyChar == '-' && InBOX.TextLength == 0)) //マイナスは入力できる
+                {
+                    e.Handled = true; //入力された値を無視
+                }
+            }
+
+            if ((e.KeyChar == (char)Keys.Enter) && InBOX.Text != "" && InBOX.Text != "-")
+            {
+                Console.WriteLine(InBOX.Text);
+                //OutBox.AppendText(InBOX.Text + "\r\n");
+                kotaeawase();
+                InBOX.Text = "";
+                if(mondaiCnt < mondaiMax)
+                {
+                    MondaiSakusei();
+                }
+                else
+                {
+                    Seiseki();
+                }
+            }
+        }
+
         private void Form1_Load(object sender, EventArgs e)
+        {
+            //MondaiSakusei(); //フォーム出力時にmondaiメソッド呼び出し
+            InBOX.Enabled = false; //回答欄を入力不可にする
+        }
+
+        //問題作成
+        private void MondaiSakusei()
+        {
+            noA = random.Next(1,10); //乱数1~10
+            noB = random.Next(1,10); //乱数1~10
+            //MondaiLbl.Text = noA + "+" + noB + "="; //乱数A＋乱数Bを出力
+            MondaiLbl.Text = noA + typeStr + noB + "=";
+            if(type == 1)
+            {
+                noC = noA + noB;
+            }
+            else
+            {
+                {
+                    noC = noA - noB;
+                }
+            }
+            //noC = noA + noB; //答え計算
+            mondaiCnt++; //出題数カウント
+        }
+
+        //答え合わせ
+        //変数と入力値を比較することで答え合わせできる
+        private void kotaeawase()
+        {
+            if(noC == Int64.Parse(InBOX.Text))
+            {
+                OutBox.AppendText("〇");
+                seikaiCnt++;
+            }
+            else
+            {
+                OutBox.AppendText("×");
+            }
+            OutBox.AppendText(MondaiLbl.Text + InBOX.Text + "\r\n");
+        }
+
+        //成績表示
+        private void Seiseki()
+        {
+            InBOX.Enabled = false;
+            MondaiLbl.Text = ""; //問題表示クリア
+            OutBox.AppendText("問題数は" + mondaiCnt + "で\r\n");
+            OutBox.AppendText("正解数は" + seikaiCnt + "でした\r\n");
+            //seikairitu = seikaiCnt / mondaiCnt * 100;
+            OutBox.AppendText("正解率は" + seikairitu + "%です。");
+        }
+
+        private void StartBtn_Click(object sender, EventArgs e)
+        {
+            //出題数変更
+            if (RB10.Checked)
+            {
+                mondaiMax = 10;
+            }
+            if (RB20.Checked)
+            {
+                mondaiMax = 20;
+            }
+            if (RB30.Checked)
+            {
+                mondaiMax = 30;
+            }
+            //計算タイプ変更
+            if(RBtasu.Checked)
+            {
+                type = 1;
+                typeStr = "+";
+            }
+            if(RBhiku.Checked)
+            {
+                type = 2;
+                typeStr = "-";
+            }
+            //mondaiMax = 5; //最大出題数
+            mondaiCnt = 0;
+            seikaiCnt = 0; //正解数カウント
+            MondaiSakusei(); //開始ボタン押下時にmondaiメソッド呼び出し
+            InBOX.Enabled = true; //入力可にする
+            InBOX.Focus(); //回答欄へフォーカスを移す
+        }
+
+        private void radioButton2_CheckedChanged(object sender, EventArgs e)
         {
 
         }
 
-        private void InBOX_KeyPress(object sender, KeyPressEventArgs e)
+        private void radioButton5_CheckedChanged(object sender, EventArgs e)
         {
-            if (e.KeyChar == (char)Keys.Enter)
-            {
-                Console.WriteLine(InBOX.Text);
-                OutBox.AppendText(InBOX.Text + "\r\n");
-                InBOX.Text = "";
-            }
+
+        }
+
+        private void groupBox2_Enter(object sender, EventArgs e)
+        {
+
         }
     }
 }
